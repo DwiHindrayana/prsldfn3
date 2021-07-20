b4ec78914497168ebeedccbbe16b4f0d1f43bd24
DwiHindrayana <87117883+DwiHindrayana@users.noreply.github.com>
2021-07-21 01:35:58 +0800

    Update admins.py

--- DaisyXMusic/function/admins.py
+++ DaisyXMusic/function/admins.py
@@ -10,4 +10,5 @@ def set(chat_id: int, admins_: List[int]):
 def get(chat_id: int) -> Union[List[int], bool]:
     if chat_id in admins:
         return admins[chat_id]
-   return False
+
+    return False
